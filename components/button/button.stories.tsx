61f154067a474ccddc76c91c631f6dd40a641db7
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-07 19:53:31 +0900

    ♻️ prettier를 모든 파일에 적용해주었습니다.

--- components/button/button.stories.tsx
+++ components/button/button.stories.tsx
@@ -1,23 +1,23 @@
 // Card.stories.ts|tsx
 
-import React from "react";
+import React from 'react';
 
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button, { LinkButton } from "./button";
+import Button, { LinkButton } from './button';
 
 export default {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Components/Button",
+  title: 'Components/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
 export const Primary: ComponentStory<typeof Button> = () => <Button></Button>;
 export const Link: ComponentStory<typeof Button> = () => {
-  return <LinkButton href={"/blogs"}></LinkButton>;
+  return <LinkButton href={'/blogs'}></LinkButton>;
 };
 export const onlick: ComponentStory<typeof Button> = () => {
   const onClick = () => {};
