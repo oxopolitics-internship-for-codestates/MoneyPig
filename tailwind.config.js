ffbf48a1c31533d59d0e83094732a7efed14b6c0
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-07 18:38:07 +0900

    🌱 📦 storybook tailwind 기본 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./elements/**/*.{js,ts,jsx,tsx}",
+    "./stories/**/*.{js,ts,jsx,tsx}",
+    "./common/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: "#9DE7D7",
+        blue: "#74D1EA",
+        violet: "#BF9BDE",
+        magenta: "#F1B2DC",
+        "light-grey": "#C1C6C8",
+        grey: "#A2AAAD",
+        black: "#101820",
+        red: "#EF3340",
+        white: "#fff",
+        pink: "#f0506e",
+      },
+    },
   },
   variants: {
     extend: {},
