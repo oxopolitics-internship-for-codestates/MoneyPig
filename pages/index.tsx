18cd6e4423a0ded78cd0f8fa8db89bdf07a16f81
Applehole <skyheo123@naver.com>
2022-10-08 13:29:06 +0900

    [Feat] OnboardingDescription

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import OnboardingDescription from '../components/OnboardingDescription/OnboardingDescription';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,13 @@ const Home: NextPage = () => {
       </Head>
 
       <div>여기는 온보딩 페이지 입니다</div>
+      <OnboardingDescription
+        description={'안녕하세요'}
+        image={
+          'http://res.heraldm.com/content/image/2015/01/19/20150119001097_0.jpg'
+        }
+        imgLocation={'right'}
+      />
     </div>
   );
 };
