66da6d0ade931ec1cf429caf5977dda4796a8f1d
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-09 23:25:03 +0900

    💄 sidebar에 absolute와 blur적용

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Sidebar from '../Navbar/Sidebar';
 
-const Layout = () => {
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
   const [menuState, setMenuState] = useState(true);
   return (
-    <>
+    <div className="sticky">
       <Header setMenuState={setMenuState} menuState={menuState} />
       <Sidebar menuState={menuState} />
-    </>
+      {children}
+    </div>
   );
 };
 
--- src/components/Navbar/Sidebar.tsx
+++ src/components/Navbar/Sidebar.tsx
@@ -7,16 +7,22 @@ type SidebarProps = {
 
 const Sidebar = ({ menuState }: SidebarProps) => {
   return (
-    <aside
-      className={`w-52 bg-grey h-screen flex flex-col items-center space-y-6 py-6 transition-all ease-in-out duration-300 ${
-        menuState ? '-translate-x-52' : 'translate-x-0'
-      } ${menuState ? 'invisible' : 'visible'}`}
+    <div
+      className={`absolute w-full h-screen bg-white/30 ${
+        menuState ? 'invisible' : 'visible'
+      } backdrop-blur-sm`}
     >
-      <LinkButton href={'#'}>{'Home'}</LinkButton>
-      <LinkButton href={'#'}>{'Make a Quiz'}</LinkButton>
-      <LinkButton href={'#'}>{'Solve a Quiz'}</LinkButton>
-      <LinkButton href={'#'}>{'Search'}</LinkButton>
-    </aside>
+      <aside
+        className={`w-52 bg-grey h-screen flex flex-col items-center space-y-6 py-6 transition-all ease-in-out duration-300 ${
+          menuState ? '-translate-x-52' : 'translate-x-0'
+        } ${menuState ? 'invisible' : 'visible'}`}
+      >
+        <LinkButton href={'#'}>{'Home'}</LinkButton>
+        <LinkButton href={'#'}>{'Make a Quiz'}</LinkButton>
+        <LinkButton href={'#'}>{'Solve a Quiz'}</LinkButton>
+        <LinkButton href={'#'}>{'Search'}</LinkButton>
+      </aside>
+    </div>
   );
 };
 
