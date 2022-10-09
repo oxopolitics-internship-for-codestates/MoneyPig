b40f4cade7c043c32114b66d9a8c3ce1de3dfaf8
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-09 17:34:00 +0900

    💄 oxQuizCard

--- src/components/Card/OxQuizCard/OxQuizCard.tsx
+++ src/components/Card/OxQuizCard/OxQuizCard.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import Card from '../Card';
+import Button from '../../Button/Button';
+
 import QuizCard from '../QuizCard/QuizCard';
 
 const OxQuizCard = () => {
-  return <QuizCard></QuizCard>;
+  return (
+    <QuizCard isOxCard={true}>
+      <div className="flex space-x-4">
+        <Button bgColor="bg-brown" style="w-1/2 text-9xl">
+          {'O'}
+        </Button>
+        <Button bgColor="bg-brown" style="w-1/2 text-9xl">
+          {'X'}
+        </Button>
+      </div>
+    </QuizCard>
+  );
 };
 
 export default OxQuizCard;
