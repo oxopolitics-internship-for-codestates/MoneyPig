4314935784e1c67de14febf91eeca253c6d465e4
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-10 17:59:56 +0900

    💄 Edit Keywords in Quiz List Card

--- src/components/Card/QuizCard/QuizCard.tsx
+++ src/components/Card/QuizCard/QuizCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { classNameJoiner } from '../../../../utils/className';
+import Timer from '../../Timer/Timer';
 import Card from '../Card';
 
 type QuizCardProps = {
@@ -12,12 +13,24 @@ type QuizCardProps = {
 const QuizCard = ({ children, isOxCard = false, quizTitle }: QuizCardProps) => {
   return (
     <Card style="space-y-4 h-128">
-      <form action="" className="space-y-8">
-        <Card bgColor="bg-white" height="h-fit" style="">
+      {quizTitle && (
+        <div className="relative">
+          <div className="bg-grey w-full h-8 rounded-full"></div>
+          <div
+            className={`animation_linear bg-pink w-full h-8 rounded-full absolute top-0 `}
+          ></div>
+        </div>
+      )}
+      <form action="" className="h-64 space-y-6">
+        <Card
+          bgColor="bg-white"
+          height="h-fit"
+          style={`${!quizTitle && 'mt-8'}`}
+        >
           {!quizTitle ? (
             <textarea
               className={classNameJoiner(
-                `w-full placeholder:text-center resize-none ${
+                `w-full placeholder:text-center text-2xl placeholder:text-2xl resize-none ${
                   isOxCard
                     ? 'h-48 placeholder:leading-12'
                     : 'h-28 placeholder:leading-7'
@@ -26,7 +39,11 @@ const QuizCard = ({ children, isOxCard = false, quizTitle }: QuizCardProps) => {
               placeholder="문제를 입력해주세요"
             ></textarea>
           ) : (
-            <h3 className="h-28 leading-7">{quizTitle}</h3>
+            <h3
+              className={`${isOxCard ? 'h-48 leading-12' : 'h-28 leading-7'}`}
+            >
+              {quizTitle}
+            </h3>
           )}
         </Card>
         {children}
