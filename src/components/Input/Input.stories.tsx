758e9dcb0936fcd9668057f6a0eb548e51b9eb97
oh min seok <oh930428@gmail.com>
2022-10-12 18:18:15 +0900

    💄 Input UI 스토리북 추가

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -8,10 +8,104 @@ export default {
   component: Input,
 } as ComponentMeta<typeof Input>;
 
-export const InputDefault: ComponentStory<typeof Input> = () => (
-  <Input type="text" placeholder="Search..." />
-);
+const InputTemplate: ComponentStory<typeof Input> = args => {
+  return <Input {...args} quiz={args.quiz} />;
+};
 
-export const InputError: ComponentStory<typeof Input> = () => (
-  <Input type="text" placeholder="Search..." isReceived={false} />
-);
+export const Default = InputTemplate.bind({});
+Default.args = {
+  type: 'text',
+  placeholder: 'Search...',
+};
+
+export const Success = InputTemplate.bind({});
+Success.args = {
+  type: 'text',
+  placeholder: 'Search...',
+  quiz: [
+    {
+      id: '1',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독점적 경쟁시장',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '2',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독점적인',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '3',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독점적',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '4',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독도',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '5',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '엔화',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '6',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독도점',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+    {
+      id: '7',
+      title: '엔화에 대한 설명으로 옳은 것을 하나 선택하시오',
+      userId: 'tmdqls2257',
+      createdAt: 1665309414,
+      answer: '',
+      time: '15s',
+      keyword: '독도도',
+      type: 'FourOptionQuiz',
+      description: '엔화는 일본의 화폐입니다.',
+    },
+  ],
+};
+
+export const Error = InputTemplate.bind({});
+Error.args = {
+  type: 'text',
+  placeholder: 'Search...',
+  quiz: [],
+};
