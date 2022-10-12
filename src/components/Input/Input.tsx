f71cf8c21244524728e996d00a6903a9d87dbdb7
oh min seok <oh930428@gmail.com>
2022-10-12 18:20:29 +0900

    Revert "✨ 키워드 유효성 검사 기능 추가"
    This reverts commit 39007f3b3f73af72a99558972043a9d7866394ce.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,73 +1,41 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { classNameJoiner } from '../../../utils/className';
+import React, { useState, useRef } from 'react';
 
 import Icon, { IconType } from '../Icon/Icon';
-
-type QuizProps = {
-  id: string;
-  title: string;
-  userId: string;
-  createdAt: number;
-  answer: string;
-  time: string;
-  keyword: string;
-  type: string;
-  description: string;
-};
 
 type InputProps = {
   type: 'text' | 'password' | 'number';
   placeholder: string;
-  quiz?: QuizProps[] | [];
+  isReceived?: boolean;
 };
 
-const Input = ({ type = 'text', placeholder, quiz }: InputProps) => {
+const Input = ({
+  type = 'text',
+  placeholder,
+  isReceived = undefined,
+}: InputProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [economyData, setEconomyData] = useState<string>('');
 
-  const [searchKeyWord, setSearchKeyWord] = useState<string>('');
-  const [isDropDownList, setIsDropDownList] = useState<boolean>(false);
-  const [filterQuiz, setFilterQuiz] = useState<QuizProps[] | undefined>(quiz);
-
-  const showDropDownList = () => {
-    if (searchKeyWord === '') {
-      setIsDropDownList(false);
-      setFilterQuiz([]);
-    } else {
-      const updateQuiz = quiz?.filter(quizList => {
-        return quizList.keyword.includes(searchKeyWord);
-      });
-      setFilterQuiz(updateQuiz);
-    }
-  };
-
-  const onSearchKeyWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchKeyWord(e.currentTarget.value);
-    setIsDropDownList(true);
-  };
-
-  const clickDropDownItem = (clickedKeyWord: string) => {
-    setSearchKeyWord(clickedKeyWord);
-    setIsDropDownList(false);
+  const onEconomyDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEconomyData(e.currentTarget.value);
   };
 
   const onReset = () => {
-    setSearchKeyWord('');
+    setEconomyData('');
     inputRef.current?.focus();
   };
 
-  useEffect(showDropDownList, [searchKeyWord, quiz]);
-
   return (
     <>
-      <div className="flex border-2 border-[#CFCFCF] rounded-[10px] bg-[#E9E7E7] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+      <div className="flex border-2 rounded-[10px] bg-[#E9E7E7] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
         <Icon style="w-12 h-12 p-3" iconName={IconType.search} />
         <input
           ref={inputRef}
           className="w-full outline-none py-3 mr-3 bg-[#E9E7E7]"
           type={type}
           placeholder={placeholder}
-          value={searchKeyWord}
-          onChange={onSearchKeyWordChange}
+          value={economyData}
+          onChange={onEconomyDataChange}
         />
         <Icon
           style="w-11 h-12 p-3"
@@ -75,40 +43,11 @@ const Input = ({ type = 'text', placeholder, quiz }: InputProps) => {
           onClick={onReset}
         />
       </div>
-
-      <>
-        {isDropDownList && (
-          <>
-            {filterQuiz && filterQuiz.length === 0 && (
-              <div className="mt-4 px-12">
-                <span className="text-[#D61616]">
-                  경제 사전에 없는 단어입니다
-                </span>
-              </div>
-            )}
-            {filterQuiz && filterQuiz.length > 0 && (
-              <ul
-                className={classNameJoiner(
-                  filterQuiz &&
-                    filterQuiz.length > 5 &&
-                    'h-60 scrollbar overflow-y-scroll overflow-x-hidden',
-                  'mt-2.5 bg-[#E9E7E7] border-2 border-[#CFCFCF] rounded-[10px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]',
-                )}
-              >
-                {filterQuiz?.map(item => (
-                  <li
-                    key={item.id}
-                    className="px-11 py-3 rounded-[10px] hover:bg-[#5D5656] hover:text-[#ffffff]"
-                    onClick={() => clickDropDownItem(item.keyword)}
-                  >
-                    {item.keyword}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </>
-        )}
-      </>
+      {isReceived === false && (
+        <div className="mt-4 px-12">
+          <span className="text-[#D61616]">경제 사전에 없는 단어입니다</span>
+        </div>
+      )}
     </>
   );
 };
