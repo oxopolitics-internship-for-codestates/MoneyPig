4672c53b3c812725f2efb7b955e6b6e109be2300
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-13 22:05:41 +0900

    💄 Change the card to the position of the center

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -6,7 +6,7 @@ import { NextPageWithLayout } from '../_app';
 
 const Quiz: NextPageWithLayout = () => {
   return (
-    <ul className="gap-4 w-full flex flex-col sm:flex-row sm:flex-wrap sm:justify-center ">
+    <ul className="gap-4 w-full flex flex-col h-112 overflow-y-scroll scrollbarHide sm:flex-row sm:flex-wrap sm:justify-center ">
       {quizList.quizes.map((quiz, idx) => (
         <li key={idx} className={'sm:w-64'}>
           <QuizListCard quiz={quiz}></QuizListCard>
