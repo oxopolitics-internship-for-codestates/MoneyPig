f42ab9387eceee4f7f70396b95a84e10729a5478
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-16 04:08:15 +0900

    🚚 rename Quiz to QuizPage

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,24 +1,33 @@
+import {
+  getFirestore,
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+} from 'firebase/firestore';
 import { observer } from 'mobx-react';
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import quizService from '../../service/QuizService';
 
 import QuizListCard from '../../src/components/Card/QuizListCard/QuizListCard';
 import Input, { InputTypeProps } from '../../src/components/Input/Input';
 import Layout from '../../src/components/Layout/Layout';
 import { quizList, UnionQuiz } from '../../src/data/QuizList';
+import { Quiz } from '../../src/store/QuizStore';
 
 import { NextPageWithLayout } from '../_app';
 
-const Quiz: NextPageWithLayout = observer(() => {
+const QuizPage: NextPageWithLayout = observer(() => {
+  const [quizes, setQuizes] = useState<Quiz[]>([]);
   useEffect(() => {
-    quizService.getQuizes();
-  });
+    quizService.getQuizes(setQuizes);
+  }, []);
   return (
     <section className="h-screen flex flex-col justify-center space-y-12">
-      <Input type={InputTypeProps.text} placeholder="키워드를 입력해주세요" />
+      {/* <Input type={InputTypeProps.text} placeholder="키워드를 입력해주세요" /> */}
 
       <ul className="gap-4 w-full flex flex-col h-112 overflow-y-scroll scrollbarHide sm:flex-row sm:flex-wrap sm:justify-center ">
-        {quizList.quizes.map((quiz, idx) => (
+        {quizes.map((quiz, idx) => (
           <li key={idx} className={'sm:w-64'}>
             <QuizListCard quiz={quiz}></QuizListCard>
           </li>
@@ -27,8 +36,8 @@ const Quiz: NextPageWithLayout = observer(() => {
     </section>
   );
 });
-Quiz.getLayout = function getLayout(page: ReactElement) {
+QuizPage.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
 
-export default Quiz;
+export default QuizPage;
--- pagesStories/QuizPage.stories.tsx
+++ pagesStories/QuizPage.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Quiz from '../pages/quiz';
+import QuizPage from '../pages/quiz';
 import Layout from '../src/components/Layout/Layout';
 import { Provider } from 'mobx-react';
 import QuizStore from '../src/store/QuizStore';
 
 export default {
   title: 'Pages/Quiz',
-  component: Quiz,
-} as ComponentMeta<typeof Quiz>;
+  component: QuizPage,
+} as ComponentMeta<typeof QuizPage>;
 
-export const MakeAQuizDefault: ComponentStory<typeof Quiz> = () => (
+export const MakeAQuizDefault: ComponentStory<typeof QuizPage> = () => (
   <Provider store={QuizStore}>
     <Layout>
-      <Quiz></Quiz>
+      <QuizPage></QuizPage>
     </Layout>
   </Provider>
 );
