0960e8cad9e6028bca4182006cee21039ba42cd3
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-19 14:55:44 +0900

    🔍 set og at detailQuiz

--- pages/quiz/[id].tsx
+++ pages/quiz/[id].tsx
@@ -6,6 +6,7 @@ import quizService from '../../service/QuizService';
 
 import { Quiz } from '../../src/store/QuizStore';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 type QuizDetailProps = {
   quiz: Quiz;
@@ -14,6 +15,28 @@ type QuizDetailProps = {
 const QuizDetail: NextPage<QuizDetailProps> = ({ quiz }: QuizDetailProps) => {
   return (
     <section className="h-screen flex flex-col justify-center space-y-12">
+      <Head>
+        <title>Economy Quiz Page</title>
+        <meta name="description" property="og:title" content="Solve A Quiz" />
+        <meta property="og:image" content="/SolveAQuiz.webp" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moneypig.vercel.app/quiz" />
+        <meta property="og:title" content="경제 퀴즈를 풀기" />
+        <meta
+          property="og:description"
+          content="경제 퀴즈를 풀고 경제에 대해 알아가보세요."
+        />
+        <meta property="og:site_name" content="Money Pig Make" />
+        <meta property="og:locale" content="ko_KR" />
+        {/* twitter */}
+        <meta name="twitter:card" content="/SolveAQuiz.webp" />
+        <meta name="twitter:title" content="경제 퀴즈를 풀기" />
+        <meta
+          name="twitter:description"
+          content="경제 퀴즈를 풀고 경제에 대해 알아가보세요."
+        />
+        <meta name="twitter:image" content="/SolveAQuiz.webp" />
+      </Head>
       {quiz.type === QuizType.oxQuiz ? (
         <OxQuizCard quiz={quiz}></OxQuizCard>
       ) : (
